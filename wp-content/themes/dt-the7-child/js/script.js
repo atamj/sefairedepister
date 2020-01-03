545d1cd00fcf4dc0392260b345dc79b40a6f67bb
atamj <jael.atam971@gmail.com>
2020-01-03 19:00:49 -0400

    script

--- wp-content/themes/dt-the7-child/js/script.js
+++ wp-content/themes/dt-the7-child/js/script.js
@@ -1,7 +1,18 @@
+function $(param){
+   jQuery(param);
+}
+
+$( "document" ).ready( function () {
+    console.log($('.elementor-tab-title'));
+
+    $('.elementor-tab-title').removeClass('elementor-active')
+} )
+
+/*
 (function($){
 
     console.log($('.elementor-tab-title'));
 
     $('.elementor-tab-title').removeClass('elementor-active')
-    
-})(jQuery);
+
+})(jQuery);*/
