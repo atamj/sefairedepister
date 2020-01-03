4039b7915b22e58574cbbd06faf751c120ad32b5
atamj <jael.atam971@gmail.com>
2020-01-03 16:36:14 -0400

    widget header

--- wp-content/themes/dt-the7-child/functions.php
+++ wp-content/themes/dt-the7-child/functions.php
@@ -3,3 +3,27 @@
  * Your code here.
  *
  */
+function add_widget_header(){
+    register_sidebar([
+       'name'               =>  esc_html__('Header colonne 1', 'the7'),
+       'id'                 =>  'header1',
+        'description'       =>  esc_html__('Ajouter des widgets', 'the7'),
+        'before_widget'     =>  "<div id='header1'>",
+        'after_widget'     =>  "</div>",
+    ]);
+    register_sidebar([
+       'name'               =>  esc_html__('Header colonne 2', 'the7'),
+       'id'                 =>  'header2',
+        'description'       =>  esc_html__('Ajouter des widgets', 'the7'),
+        'before_widget'     =>  "<div id='header2'>",
+        'after_widget'     =>  "</div>",
+    ]);
+    register_sidebar([
+       'name'               =>  esc_html__('Header colonne 3', 'the7'),
+       'id'                 =>  'header3',
+        'description'       =>  esc_html__('Ajouter des widgets', 'the7'),
+        'before_widget'     =>  "<div id='header3'>",
+        'after_widget'     =>  "</div>",
+    ]);
+}
+add_action('widgets_init', 'add_widget_header');
