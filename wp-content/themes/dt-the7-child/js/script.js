2bd65b0c0dc95862d5486b58ee5faa5061dadb29
atamj <jael.atam971@gmail.com>
2020-01-03 19:11:11 -0400

    script

--- wp-content/themes/dt-the7-child/js/script.js
+++ wp-content/themes/dt-the7-child/js/script.js
@@ -2,5 +2,7 @@
 jQuery( "document" ).ready( function ($) {
     console.log($('.elementor-tab-title'));
 
-    $('.elementor-tab-title')[0].removeClass('elementor-active')
+    /*$('.elementor-tab-title')[0].removeClass('elementor-active')*/
+    $('.elementor-tab-title').forEach(elm => elm.removeClass('elementor-active'))
+
 } )
