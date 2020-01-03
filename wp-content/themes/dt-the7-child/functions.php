6dd60a060426af43bd1597d7f01e24feed8d1cd5
atamj <jael.atam971@gmail.com>
2020-01-03 18:34:40 -0400

    script

--- wp-content/themes/dt-the7-child/functions.php
+++ wp-content/themes/dt-the7-child/functions.php
@@ -27,3 +27,8 @@ function add_widget_header(){
     ]);
 }
 add_action('widgets_init', 'add_widget_header');
+
+function add_script(){
+    wp_enqueue_script('script', plugins_url('js/script.js', __FILE__), ['jquery'], '1.2.0', true);
+}
+add_action('wp_enqueue_scripts', ('add-script'));
