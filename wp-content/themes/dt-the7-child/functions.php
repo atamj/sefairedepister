9b66dc338144e490c841ae3ed4b664cbe04cc656
atamj <jael.atam971@gmail.com>
2020-01-04 18:35:40 -0400

    owl

--- wp-content/themes/dt-the7-child/functions.php
+++ wp-content/themes/dt-the7-child/functions.php
@@ -28,9 +28,12 @@ function add_widget_header(){
 }
 add_action('widgets_init', 'add_widget_header');
 
-function add_script(){
+function add_script_style(){
 
     wp_enqueue_script('script',get_template_directory_uri().'-child/js/script.js', ['jquery'], '1.2.0', true);
+    wp_enqueue_script('owl',get_template_directory_uri().'-child/OwlCarousel/owl-carousel/owl.carousel.min.js',['jquery'], '2.0', true);
+    wp_enqueue_style( 'owl.carousel', get_template_directory_uri().'-child/OwlCarousel/owl-carousel/owl.carousel.css');
+    wp_enqueue_style( 'owl.transitions', get_template_directory_uri().'-child/OwlCarousel/owl-carousel/owl.transitions.css');
 
 }
-add_action('init', ('add_script'));
+add_action('init', ('add_script_style'));
