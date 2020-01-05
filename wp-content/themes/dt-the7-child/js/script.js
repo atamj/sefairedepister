8f042d79f255e8bb17b7485ebd282cbef5c84629
atamj <jael.atam971@gmail.com>
2020-01-04 20:47:34 -0400

    btn

--- wp-content/themes/dt-the7-child/js/script.js
+++ wp-content/themes/dt-the7-child/js/script.js
@@ -3,7 +3,7 @@ jQuery( "document" ).ready( function ($) {
 
 	$("#next").click(function(event) {
 		$(this).toggle()
-		$(".owl-next").click();
+		$(".black .sa_owl_theme .owl-nav .owl-next").click();
 	});
 
 } )
