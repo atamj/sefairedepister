2b8ca9c383dd20f5d5d76a8663a8924193d571d4
atamj <jael.atam971@gmail.com>
2020-01-04 21:33:24 -0400

    btn

--- wp-content/themes/dt-the7-child/js/script.js
+++ wp-content/themes/dt-the7-child/js/script.js
@@ -8,8 +8,12 @@ jQuery( "document" ).ready( function ($) {
 	$(".btn[data-toggle]").click(function(e) {
 		e.preventDefault()
 		let data = $(this).data('toggle')
-		$(this).parent('div').children(data).toggle()
+		console.log("data= " + data)
+		let div_to_toggle = $(this).parent('div').children(data)
+		console.log("div_to_toggle= " + div_to_toggle)
 		
+		div_to_toggle.toggle()
+
 		// if ($(this).data('toggle') == "yes") 
 		// {
 		// 	$(this).parent('div').children('.yes').toggle()
