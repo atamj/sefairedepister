9910501b2d07a029ccd98d9cab46687d39d42781
atamj <jael.atam971@gmail.com>
2020-01-08 22:32:07 -0400

    btn slider

--- wp-content/themes/dt-the7-child/js/script.js
+++ wp-content/themes/dt-the7-child/js/script.js
@@ -3,7 +3,7 @@ jQuery( "document" ).ready( function ($) {
 
 	$("#next, .next").click(function(e) {
 		e.preventDefault()
-		$(".black .sa_owl_theme .owl-nav .owl-next").click();
+		// $(".black .sa_owl_theme .owl-nav .owl-next").click();
 		$(".ls-gui-element.ls-nav-next").click();
 	});
 	$(".btn[data-toggle]").click(function(e) {
@@ -19,10 +19,6 @@ jQuery( "document" ).ready( function ($) {
 
 		div_to_toggle.toggle()
 
-		// if ($(this).data('toggle') == "yes") 
-		// {
-		// 	$(this).parent('div').children('.yes').toggle()
-		// }
 		
 	});
 
